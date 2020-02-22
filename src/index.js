26c6172aca21004bcebe6813180a6361e37cf414
Alex Geer <geer.alex@gmail.com>
2020-02-21 18:11:52 -0800

    removed deprecated images imports that were failing build

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import img4 from '../assets/images/express.png'
 import img5 from '../assets/images/mysql.png'
 import img6 from '../assets/images/node.png'
 import img7 from '../assets/images/react.png'
-import img8 from '../assets/images/camping.jpg'
-import img9 from '../assets/images/ridge.jpg'
 
 import pdf1 from '../assets/pdf/AlexGeerResume.pdf'
 
