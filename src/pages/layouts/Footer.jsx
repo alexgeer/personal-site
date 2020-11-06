a866c2402170be27aaba483d762397176ce99c80
Alex Geer <geer.alex@gmail.com>
2020-11-06 14:06:45 -0500

    centered footer items

--- src/pages/layouts/Footer.jsx
+++ src/pages/layouts/Footer.jsx
@@ -2,31 +2,41 @@ import React from "react";
 import styled from "styled-components";
 
 const FooterContainer = styled.footer`
- display: flex;
- flex-direction: column;
- align-items: center;
- justify-content: space-around;
- margin: 0px auto;
- padding: 10px 0px 10px 0px;
- background: ${ ({theme}) => theme.navBackground };
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  margin: 0px auto;
+  padding: 10px 0px 10px 0px;
+  background: ${({ theme }) => theme.navBackground};
 
   img {
     height: auto;
     width: 50px;
+    border-radius: 100%
   }
 
-  img, a {
-          padding: 5px 0;
-      }
-  @media only screen and (min-width: 600px){
-      flex-direction: row;
-     
+  a {
+    text-align: center;
+    width: 200px;
+    display: flex;
+    justify-content: center;
+    padding: 5px 0;
+
+  }
+
+  span {
+    
+  }
+
+  @media only screen and (min-width: 600px) {
+    flex-direction: row;
   }
 `;
 
 const Footer = () => {
   return (
-    <FooterContainer id='footer'>
+    <FooterContainer id="footer">
       <a href="mailto:geer.alex@gmail.com">geer.alex@gmail.com</a>
       <a href="https://github.com/alexgeer">
         <img src="images/github.png" />
